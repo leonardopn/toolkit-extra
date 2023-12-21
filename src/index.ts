385e15b9896edab646e76b5bb858687e0fac185a
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-12-21 15:25:16 -0300

    feat(Module: Timeout): :sparkles: Cria e exporta funções para o módulo

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { deleteDuplicate, generateArray, getMinMax, repeatArray, isArrayTyped, m
 export { isClassInstance, omitProp, omitUndefinedProps } from "./modules/Object";
 export { getRandomNumber } from "./modules/Random";
 export { capitalizeFirstLetterOfEachWord, formatCNPJ, getNameInitials, hideEmail, isUrl } from "./modules/String";
+export { sleep, timeoutAsync } from "./modules/Timeout";
