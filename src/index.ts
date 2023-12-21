80165c63a21caf84c08b1f23bfcb003750ea3787
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-12-21 15:15:36 -0300

    feat(Module: Object): :sparkles: Exporta o módulo de objetos

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { deleteDuplicate, generateArray, getMinMax, repeatArray, isArrayTyped, moveElement } from "./modules/Array";
+export { isClassInstance, omitProp, omitUndefinedProps } from "./modules/Object";
 export { capitalizeFirstLetterOfEachWord, formatCNPJ, getNameInitials, hideEmail, isUrl } from "./modules/String";
